0d027118dbb34a6bbaca9a08dda0bcf65ee608e3
Jerin Lee <taurenjerin@qq.com>
2017-05-31 20:36:55 +0800

    initialize .net code.
    Signed-off-by: Jerin Lee <taurenjerin@qq.com>

---
+++ dotnet/iAccount/iAccount.Core/Interfaces/IUser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iAccount.Core.Interfaces {
+  /// <summary>
+  /// T: ulong, string, GUID/UUID
+  /// </summary>
+  /// <typeparam name="T"></typeparam>
+  public interface IUser<T> {
+    /// <summary>
+    /// unique id
+    /// </summary>
+    T Id { get; set; }
+    /// <summary>
+    /// unique user name
+    /// </summary>
+    string UserName { get; set; }
+
+  }
+
+}
